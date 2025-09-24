b9dad45942be37964f0c7f4ce4fdfd77d89dad56
Opilane TTHK <opilane@tthk.ee>
2025-09-24 10:40:51 +0300

    ifelseifelse

---
+++ MinuKonspekt/MinuKonspekt/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace MinuKonspekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Tere kasutaja, mis on sinu nimi");
+            string nimi = Console.ReadLine();
+
+            if (nimi == "")
+            {
+                Console.WriteLine("Kasutaja ei sisestanud oma nime");
+            }
+            else if (nimi != "")
+            {
+                Console.WriteLine("Tere " + nimi + "!");
+            }
+            else
+            {
+                Console.WriteLine("Kasutaja ei sisestanud oma nime");
+            }
+
+            Console.WriteLine(nimi + ", mis on sinu vanus?: ");
+            int kasutajavanus = int.Parse(Console.ReadLine());
+            //if (kasutajavanus > 0)
+            //{
+            //    if (kasutajavanus < 18)
+            //    {
+            //        Console.WriteLine("Kahjuks energiajooki sulle ei müüda, oled Monsterist ilma 😒");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Saad osta Monsterit");
+            //    }
+            //}
+            if (kasutajavanus > 0 && kasutajavanus < 18)
+            {
+                Console.WriteLine("Kahjuks energiajooki sulle ei müüda, oled Monsterist ilma 😒");
+            }
+            else
+            {
+                Console.WriteLine("Saad osta Monsterit");
+            }
+            Console.WriteLine("Sisesta oma pikkus ka, palun, " + nimi + ":");
+            double pikkus = double.Parse(Console.ReadLine());
+            if (pikkus < 1.00d)
+            {
+                Console.WriteLine("Oled  juntsu");  
+            }
+            else if (pikkus <1.25d && pikkus > 1.00d)
+            {
+                Console.WriteLine("Oled peaaegu allameetrimees");
+            }
+            else if (pikkus <1.50d && pikkus >1.25d)
+            {
+                Console.WriteLine("Oioi, enam ei olegi "+nimi+" !");
+            }
+            else if (pikkus <1.75d && pikkus >1.50d)
+            {
+                Console.WriteLine("Oled ennasti standardpikkuses, kui mitte just natuke lühike");
+            }
+            else if (pikkus < 2.00d && pikkus > 1.75d)
+            {
+                Console.WriteLine("Pikk kolge, vaata et sa pead vastu uksepiita ära ei löö");
+            }
+            else if (pikkus > 2.00d)
+            {
+                Console.WriteLine("Täielik tulnukas, kuidas pilved välja näevad?");
+            }
+            else
+            {
+                Console.WriteLine("pikkust ei tunta");
+            }
+        }
+    }
+}
